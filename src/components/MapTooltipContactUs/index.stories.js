0e181a4d574ac6d6df5d9081ec961885826b5e0d
flavia.martini <flavia.martini@ahgora.com.br>
2020-03-26 16:42:53 -0300

    first commit

---
+++ src/components/MapTooltipContactUs/index.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { MapTooltipContactUs } from 'components'
+
+storiesOf('MapTooltipContactUs', module)
+  .add('default', () => (
+    <MapTooltipContactUs />
+  ))
